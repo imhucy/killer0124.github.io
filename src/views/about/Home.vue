109610328fc735b30a842f2ad7b2580849b87a07
胡超宇 <huchaoyu987@163.com>
2017-06-08 15:29:23 +0800

    v-for :key

--- src/views/about/Home.vue
+++ src/views/about/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-            <el-menu-item :index="index" v-for="(item, index) in navData">{{item.text}}</el-menu-item>
+            <el-menu-item :index="index" v-for="(item, index) in navData" :key="index">{{item.text}}</el-menu-item>
             <!-- <el-submenu index="2">
                 <template slot="title">我的工作台</template>
                 <el-menu-item index="2-1">选项1</el-menu-item>
